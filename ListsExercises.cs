358e03185442d8dc0579357dce9269e4284738b3
RodriguezOswaldo <owner2303@gmail.com>
2019-10-16 14:17:14 -0400

    ok

---
+++ ListsExercises.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lists
+{
+    class ListsExercises
+    {
+        public static void Users()
+        {
+            var names = new List<string>();
+
+            while (true)
+            {
+                Console.WriteLine("type a name: (or hit Enter to quit): ");
+
+                var input = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(input))
+                    break;
+                names.Add(input);
+            }
+        }
+    }
+}
