6fc08e72f9e9de1567858aa56debdb75becb566b
RodriguezOswaldo <owner2303@gmail.com>
2019-10-16 14:19:01 -0400

    list

--- ListsExercises.cs
+++ ListsExercises.cs
@@ -19,6 +19,15 @@ namespace Lists
                     break;
                 names.Add(input);
             }
+            if (names.Count > 2)
+                Console.WriteLine("{0}, {1} and {2} others like your post", names[0], names[1], names.Count - 2);
+            else if (names.Count == 2)
+                Console.WriteLine("{0} and {1} like your post", names[0], names[1]);
+            else if (names.Count == 1)
+                Console.WriteLine("{0} likes your post.", names[0]);
+            else
+                Console.WriteLine();
         }
     }
-}
+    }
+
